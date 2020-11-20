76b0230f07e94ba7ba470a31bfda484632b55b2e
guillaume bodart <guillaume.bo@hotmail.com>
2020-11-20 09:59:15 +0100

    ok

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,10 @@
+class IngredientsController < ApplicationController
+
+# @ingredient = Ingredient.new(params[:ingredient_id])
+#     if @ingredient.save
+#     redirect_to cocktail_path(@cocktail)
+#     else
+#     render "new"
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "ginger ale")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
